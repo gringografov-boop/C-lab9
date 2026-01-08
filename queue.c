c57a8499f1224acf0546fe89d018c57dd8dd76e0
grafov <gringo.grafov@gmail.com>
2026-01-08 23:26:58 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "queue.h"
 #include <stdio.h>
-#include <string.h>
 
 void queue_create(queue* q) {
     q->head = 0;
@@ -19,7 +18,7 @@ size_t queue_size(const queue* q) {
 
 void queue_push_back(queue* q, const data_type elem) {
     if (q->count >= 100) {
-        fprintf(stderr, "Ошибка: очередь переполнена!\n");
+        fprintf(stderr, "РћС€РёР±РєР°: РѕС‡РµСЂРµРґСЊ РїРµСЂРµРїРѕР»РЅРµРЅР°!\n");
         return;
     }
 
@@ -30,7 +29,7 @@ void queue_push_back(queue* q, const data_type elem) {
 
 void queue_push_front(queue* q, const data_type elem) {
     if (q->count >= 100) {
-        fprintf(stderr, "Ошибка: очередь переполнена!\n");
+        fprintf(stderr, "РћС€РёР±РєР°: РѕС‡РµСЂРµРґСЊ РїРµСЂРµРїРѕР»РЅРµРЅР°!\n");
         return;
     }
 
@@ -41,7 +40,7 @@ void queue_push_front(queue* q, const data_type elem) {
 
 void queue_pop_front(queue* q) {
     if (queue_is_empty(q)) {
-        fprintf(stderr, "Ошибка: очередь пуста!\n");
+        fprintf(stderr, "РћС€РёР±РєР°: РѕС‡РµСЂРµРґСЊ РїСѓСЃС‚Р°!\n");
         return;
     }
 
@@ -51,119 +50,99 @@ void queue_pop_front(queue* q) {
 
 void queue_pop_back(queue* q) {
     if (queue_is_empty(q)) {
-        fprintf(stderr, "Ошибка: очередь пуста!\n");
+        fprintf(stderr, "РћС€РёР±РєР°: РѕС‡РµСЂРµРґСЊ РїСѓСЃС‚Р°!\n");
         return;
     }
 
     q->count--;
 }
 
-data_type queue_at(const queue* q, size_t pos) {
-    data_type result;
-    result = q->data[(q->head + pos) % 100];
-    return result;
+data_type queue_front(const queue* q) {
+    return q->data[q->head];
 }
 
-void queue_set(queue* q, size_t pos, const data_type elem) {
-    q->data[(q->head + pos) % 100] = elem;
-}
-
-void queue_print_recursive(const queue* q, size_t pos) {
+static void queue_print_recursive_impl(const queue* q, size_t pos) {
     if (pos >= q->count) {
         printf("\n");
         return;
     }
 
-    data_type elem = queue_at(q, pos);
+    size_t idx = (q->head + pos) % 100;
+    data_type elem = q->data[idx];
+
     printf("[key=%d, val=%d] ", elem.key, elem.value);
 
-    queue_print_recursive(q, pos + 1);
+    queue_print_recursive_impl(q, pos + 1);
+}
+
+void queue_print_recursive(const queue* q, size_t pos) {
+    queue_print_recursive_impl(q, pos);
 }
 
 void queue_print(const queue* q) {
     if (queue_is_empty(q)) {
-        printf("Очередь пуста\n");
+        printf("РћС‡РµСЂРµРґСЊ РїСѓСЃС‚Р°\n");
         return;
     }
 
-    printf("Очередь (%lu элементов): ", q->count);
-    queue_print_recursive(q, 0);
+    printf("РћС‡РµСЂРµРґСЊ (%lu СЌР»РµРјРµРЅС‚РѕРІ): ", (unsigned long)q->count);
+    queue_print_recursive_impl(q, 0);
 }
 
 void queue_insert(queue* q, const data_type elem) {
     if (q->count >= 100) {
-        fprintf(stderr, "Ошибка: очередь переполнена!\n");
+        fprintf(stderr, "РћС€РёР±РєР°: РѕС‡РµСЂРµРґСЊ РїРµСЂРµРїРѕР»РЅРµРЅР°!\n");
         return;
     }
 
-    size_t insert_pos = 0;
-    while (insert_pos < q->count) {
-        data_type current = queue_at(q, insert_pos);
-        if (current.key >= elem.key) {
-            break;
-        }
-        insert_pos++;
-    }
-
-    q->count++;
-
-    for (size_t i = q->count - 1; i > insert_pos; i--) {
-        data_type temp = queue_at(q, i - 1);
-        queue_set(q, i, temp);
-    }
-
-    queue_set(q, insert_pos, elem);
-}
-
-void queue_erase(queue* q, key_type key) {
     size_t pos = 0;
-
     while (pos < q->count) {
-        data_type current = queue_at(q, pos);
-        if (current.key == key) {
-            for (size_t i = pos; i < q->count - 1; i++) {
-                data_type temp = queue_at(q, i + 1);
-                queue_set(q, i, temp);
-            }
-            q->count--;
-            return;
+        size_t idx = (q->head + pos) % 100;
+        data_type cur = q->data[idx];
+        if (cur.key >= elem.key) {
+            break;
         }
         pos++;
     }
 
-    fprintf(stderr, "Ошибка: элемент с ключом %d не найден!\n", key);
-}
-
-iterator queue_begin(queue* q) {
-    iterator it;
-    it.pos = 0;
-    it.cont = q;
-    return it;
-}
-
-iterator iter_next(iterator it) {
-    if (it.pos < it.cont->count) {
-        it.pos++;
+    size_t i = q->count;
+    while (i > pos) {
+        size_t from = (q->head + i - 1) % 100;
+        size_t to   = (q->head + i) % 100;
+        q->data[to] = q->data[from];
+        i--;
     }
-    return it;
+
+    size_t insert_idx = (q->head + pos) % 100;
+    q->data[insert_idx] = elem;
+    q->count++;
 }
 
-iterator iter_prev(iterator it) {
-    if (it.pos > 0) {
-        it.pos--;
+void queue_erase(queue* q, key_type key) {
+    if (queue_is_empty(q)) {
+        fprintf(stderr, "РћС€РёР±РєР°: РѕС‡РµСЂРµРґСЊ РїСѓСЃС‚Р°!\n");
+        return;
     }
-    return it;
-}
 
-data_type* iter_deref(iterator it) {
-    if (it.pos < it.cont->count) {
-        static data_type elem;
-        elem = queue_at(it.cont, it.pos);
-        return &elem;
+    size_t pos = 0;
+    while (pos < q->count) {
+        size_t idx = (q->head + pos) % 100;
+        data_type cur = q->data[idx];
+
+        if (cur.key == key) {
+            size_t i = pos;
+            while (i + 1 < q->count) {
+                size_t from = (q->head + i + 1) % 100;
+                size_t to   = (q->head + i) % 100;
+                q->data[to] = q->data[from];
+                i++;
+            }
+            q->count--;
+            return;
+        }
+
+        pos++;
     }
-    return NULL;
-}
 
-bool iter_valid(iterator it) {
-    return it.pos < it.cont->count;
+    fprintf(stderr, "РћС€РёР±РєР°: СЌР»РµРјРµРЅС‚ СЃ РєР»СЋС‡РѕРј %d РЅРµ РЅР°Р№РґРµРЅ!\n", key);
 }
