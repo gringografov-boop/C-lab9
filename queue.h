c36b4a45d293eebeff7a2777c401dcd57d0cac1e
grafov <gringo.grafov@gmail.com>
2026-01-13 17:37:32 +0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -15,27 +15,19 @@ typedef struct {
 typedef struct {
     data_type data[100];
     size_t head;
-    size_t tail;
     size_t count;
 } queue;
 
-void queue_create(queue* q);
-bool queue_is_empty(const queue* q);
+void   queue_create(queue* q);
+void   queue_destroy(queue* q);
 
-void queue_push_back(queue* q, const data_type elem);
-void queue_push_front(queue* q, const data_type elem);
-
-void queue_pop_front(queue* q);
-void queue_pop_back(queue* q);
-
+bool   queue_is_empty(const queue* q);
 size_t queue_size(const queue* q);
 
-data_type queue_front(const queue* q);
+bool   queue_push(queue* q, const data_type elem);
+bool   queue_pop(queue* q, data_type* out);
+bool   queue_front(const queue* q, data_type* out);
 
-void queue_print(const queue* q);
-void queue_print_recursive(const queue* q, size_t pos);
-
-void queue_insert(queue* q, const data_type elem);
-void queue_erase(queue* q, key_type key);
+void   queue_print(const queue* q);
 
 #endif
